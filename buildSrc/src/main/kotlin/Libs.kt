15704a0d35cdee758539c9890ca8fe1ce4e1f82b
Maxi Rosson <maxirosson@gmail.com>
2022-07-25 12:00:23 -0300

    Add kotlin configuration + more dipien plugins

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -1,5 +1,8 @@
 object Libs {
 
+    // https://github.com/JetBrains/kotlin/blob/master/ChangeLog.md
+    const val KOTLIN = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.5.31"
+
     // https://github.com/junit-team/junit4/releases
     const val JUNIT = "junit:junit:4.13.2"
 
